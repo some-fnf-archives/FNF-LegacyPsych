53146eaca95b5a34fcdee6b97a154c8f63245088
crowplexus <lucariothelast91@gmail.com>
2023-11-29 02:49:32 -0400

    fix compiling error
    mah baaadddd

--- source/options/VisualsUISubState.hx
+++ source/options/VisualsUISubState.hx
@@ -37,8 +37,7 @@ class VisualsUISubState extends BaseOptionsMenu
 		var option:Option = new Option('Note Splashes',
 			"If unchecked, hitting \"Sick!\" notes won't show particles.",
 			'noteSplashes',
-			'bool',
-			true);
+			'bool');
 		addOption(option);
 
 		var option:Option = new Option('Hide HUD',
